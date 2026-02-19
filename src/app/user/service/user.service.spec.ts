15fc1bfe296770857b64ff0c3241cd507701b897
Allan <allanalcarsou2@gmail.com>
2026-02-18 23:27:46 -0300

    test: add unit tests for UserService and UserController

---
+++ src/app/user/service/user.service.spec.ts
@@ -0,0 +1,43 @@
+import { Test } from "@nestjs/testing";
+import { UserRepository } from "src/domain/repositories/user.repository";
+import { UserService } from "./user.service";
+import { EUserRole } from "src/utils/enum/user-role.enum";
+
+describe("Suite Test UserService", () => {
+  let service: UserService;
+  let userRepository: UserRepository;
+
+  const mockUserRepo = { findById: jest.fn() };
+  beforeEach(async () => {
+    const module = await Test.createTestingModule({
+      providers: [
+        UserService,
+        {
+          provide: UserRepository,
+          useValue: mockUserRepo
+        }
+      ],
+    }).compile();
+
+    service = module.get<UserService>(UserService);
+    userRepository = module.get<UserRepository>(UserRepository);
+
+    jest.clearAllMocks();
+  });
+
+  describe("test me method", () => {
+    it("should return user data", async () => {
+      const payload = { 
+        id: 1, 
+        name: "John Doe", 
+        email: "john.doe@example.com",
+        role: EUserRole.ADMIN 
+      };
+      mockUserRepo.findById.mockResolvedValue(payload);
+
+      const result = await service.me(payload);
+      expect(result).toEqual(payload);
+      expect(mockUserRepo.findById).toHaveBeenCalledWith(payload.id);
+    });
+  });
+});
